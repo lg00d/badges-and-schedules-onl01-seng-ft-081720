5f12dd8a4ec6a141a4ce51c1ef6321102197a1cc
elizabeth goodman <egoodman1180@icloud.com>
2020-08-24 17:49:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  result = []
+  names.each do |name|
+    result.push(badge_maker(name))
+  end
+  return result
+end
+
+def assign_rooms(names)
+  result = []
+  names.each_with_index do |name, index|
+    result.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+  return result
+end
+    
+def printer
+  puts(batch_badge_creator(names))
+    return results
+  puts(assign_rooms)
+    return results
+  end
+end
+  
